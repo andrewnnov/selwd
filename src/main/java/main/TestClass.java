1ca91fcd0cabd365482d1f8820734515ba736522
andreybannikov <andrewnnov@yandex.ru>
2020-10-18 11:51:10 +0300

    work with checkbox

--- src/main/java/main/TestClass.java
+++ src/main/java/main/TestClass.java
@@ -16,8 +16,8 @@ public class TestClass {
         System.setProperty("webdriver.chrome.driver", "C:\\Projects\\selwd\\driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         //неявное ожидание
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.get("https://en.wikipedia.org/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.get("https://en.wikipedia.org/");
         //управление размером экрана
         driver.manage().window().maximize();
 
@@ -26,14 +26,46 @@ public class TestClass {
 //        //get text field
 //        System.out.println(driver.findElement(By.xpath("//div[@id='searchText']/input")).getAttribute("value"));
 //        driver.findElement(By.xpath("//div[@id='searchText']/input")).clear();
+        //driver.get("https://yandex.ru/");
+        //System.out.println(driver.getWindowHandle());
 
-        WebElement link = driver.findElement(By.xpath("//li[@id='n-aboutsite']/a"));
-        System.out.println(link.getText());
-        link.click();
+//
+//        driver.findElement(By.xpath("//div[text()='Маркет']")).click();
+//
+//        for (String tab : driver.getWindowHandles()) {
+//           driver.switchTo().window(tab);
+//            //System.out.println(tab);
+//        }
 
-        driver.get("https://www.instagram.com/");
-        WebElement linkGit = driver.findElement(By.xpath("(//li[@class='K5OFK']/a)[2]"));
-        linkGit.click();
+
+
+        //System.out.println(driver.getWindowHandle());
+
+        //WebElement webElement1 = driver.findElement(By.xpath("//div[@class='_3t-zItniCq']//button"));
+        //webElement1.submit();
+        //driver.findElement(By.xpath("//button/span[text()='Да, спасибо']")).click();
+//        if(elementGeoLocation.isEnabled()) {
+//            elementGeoLocation.click();
+//        }
+        //elementGeoLocation.click();
+
+        driver.get("https://www.udemy.com/");
+
+        driver.findElement(By.xpath("//div[@class='header--gap-auth-button--7KoL0']")).click();
+        //driver.findElement(By.xpath("//span[text()='Бытовая техника']")).click();
+
+
+        driver.findElement(By.xpath("//input[@id='email--1']")).sendKeys("*******");
+        driver.findElement(By.xpath("//input[@id='id_password']")).sendKeys("******");
+        driver.findElement(By.xpath("//input[@id='submit-id-submit']")).submit();
+        driver.findElement(By.xpath("//span[@class='udlite-text-sm header--dropdown-button-text--2jtIM' and text()='Мое обучение']")).click();
+        driver.findElement(By.xpath("//strong[@class='details__name' and text()='Full Stack: Angular and Spring Boot']")).click();
+        driver.findElement(By.xpath("//div[@data-purpose='curriculum-item-9-2']/label[@class='curriculum-item-link--progress-toggle--1CMcg checkbox-inline']/input")).click();
+        System.out.println(driver.findElement(By.xpath("//div[@data-purpose='curriculum-item-9-2']/label[@class='curriculum-item-link--progress-toggle--1CMcg checkbox-inline']/input")).isSelected());
+        driver.findElement(By.xpath("//div[@data-purpose='curriculum-item-9-2']/label[@class='curriculum-item-link--progress-toggle--1CMcg checkbox-inline']/input")).click();
+
+        System.out.println(driver.findElement(By.xpath("//div[@data-purpose='curriculum-item-9-2']/label[@class='curriculum-item-link--progress-toggle--1CMcg checkbox-inline']/input")).isSelected());
+
 
 
 
