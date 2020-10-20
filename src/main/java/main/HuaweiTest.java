7fd24f21a179ca52397e6ccf07f084f2dd0a6665
andreybannikov <andrewnnov@yandex.ru>
2020-10-20 08:26:49 +0300

    explicit wait

--- src/main/java/main/HuaweiTest.java
+++ src/main/java/main/HuaweiTest.java
@@ -3,6 +3,9 @@ package main;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,10 +14,13 @@ public class HuaweiTest {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Projects\\selwd\\driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
         driver.get("https://support.microsoft.com/ru-ru");
+
+        WebDriverWait wait = (new WebDriverWait(driver, 5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h1[contains(text(), \"Добро пожаловать в службу поддержки Майкрософт!\")]")));
 
         driver.findElement(By.xpath("//a[@class='ocpExternalLink supHomeAndLandingPageCTA' and text()='ПРОСМОТРЕТЬ ВАРИАНТЫ ОБУЧЕНИЯ']")).click();
 
