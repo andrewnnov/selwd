e8cc66ce762fcfe22855638677499c5f8558a76e
andreybannikov <andrewnnov@yandex.ru>
2020-10-20 11:24:25 +0300

    check that element is presented on page

--- src/main/java/main/HuaweiTest.java
+++ src/main/java/main/HuaweiTest.java
@@ -19,33 +19,13 @@ public class HuaweiTest {
         driver.manage().window().maximize();
 
         driver.get("https://www.labirint.ru/");
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
-        jse.executeScript("alert('Are you sure?');");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+        if(driver.findElements(By.xpath("//a")).size() > 0) {
+            System.out.println("This element is presented");
         }
 
-        driver.switchTo().alert().accept(); //принять OK
-        //driver.switchTo().alert().dismiss(); //отказаться если есть NO
 
 
-//        JavascriptExecutor jse = (JavascriptExecutor)driver;
-        //jse.executeScript("alert('Hello');");
-
-//        jse.executeScript("window.scrollBy(0, 10000)", "");
-//        jse.executeScript("window.scrollBy(0, -10000)", "");
-
-//        driver.get("https://www.labirint.ru/");
-//        WebElement link = driver.findElement(By.xpath("//a[text()='Книги']"));
-//        WebElement element = driver.findElement(By.xpath("//a[text()='Книги']"));
-//        Actions actions = new Actions(driver);
-//        actions.moveToElement(link).build().perform();
-//
-//        actions.dragAndDrop(element, link).build().perform();
-//
-//        actions.clickAndHold(element).moveToElement(link).release().build().perform();
 
 
 
