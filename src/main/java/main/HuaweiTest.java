72ec53bd49da2579e06f795624005fa9f03cdecf
andreybannikov <andrewnnov@yandex.ru>
2020-10-20 19:06:23 +0300

    work with keyboard

--- src/main/java/main/HuaweiTest.java
+++ src/main/java/main/HuaweiTest.java
@@ -18,11 +18,25 @@ public class HuaweiTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        driver.get("https://www.labirint.ru/");
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
 
-        if(driver.findElements(By.xpath("//a")).size() > 0) {
-            System.out.println("This element is presented");
-        }
+        WebElement searchInput = driver.findElement(By.xpath("//input[@id='searchInput']"));
+        //searchInput.sendKeys("text");
+        String select = Keys.chord(Keys.CONTROL, "a");
+        String cut = Keys.chord(Keys.CONTROL, "x");
+        String paste = Keys.chord(Keys.CONTROL, "v");
+
+
+        searchInput.sendKeys("test text");
+        searchInput.sendKeys(select);
+        searchInput.sendKeys(cut);
+        searchInput.sendKeys(paste);
+        searchInput.sendKeys(Keys.ENTER);
+
+//        searchInput.sendKeys(Keys.chord(Keys.LEFT_SHIFT, "test with shift"));
+//        searchInput.sendKeys(Keys.ENTER);
+
+
 
 
 
