d02477957da87d0b8514b7c9be7bc5a958e7018b
andreybannikov <andrewnnov@yandex.ru>
2020-10-29 22:53:49 +0300

    practice xpass and css selector

--- src/main/java/locators/LocatorExample.java
+++ src/main/java/locators/LocatorExample.java
@@ -13,32 +13,20 @@ public class LocatorExample {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.facebook.com/");
-        //driver.findElement(By.id("email")).sendKeys("test@test.com");
-        driver.findElement(By.cssSelector("#email")).sendKeys("emailAddress");
-        driver.findElement(By.name("pass")).sendKeys("123456");
-        driver.findElement(By.linkText("Забыли пароль?")).click();
-        //driver.close();
+
+        driver.findElement(By.xpath("//form[@class='_featuredLogin__formContainer']//a[@role='button']")).click();
+        driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("Name");
+        driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("lastName");
+
+
+        driver.get("https://www.facebook.com/");
+        //tagName[attribute='value']
+        driver.findElement(By.cssSelector("form a[role='button']")).click();
+        driver.findElement(By.cssSelector("input[name='firstname']")).sendKeys("NameCSS");
+        driver.findElement(By.cssSelector("input[name='lastname']")).sendKeys("lastNameCSS");
 
 
 
-        driver.get("https://login.salesforce.com/");
-        driver.findElement(By.id("username")).sendKeys("Hello");
-        driver.findElement(By.name("pw")).sendKeys("123456");
-        //driver.findElement(By.id("Login")).click();
-
-        driver.findElement(By.xpath("//input[@id='Login']")).click();
-        Thread.sleep(6000);
-
-
-        String cssText = driver.findElement(By.cssSelector("div#error.loginError")).getText();
-        String cssText2 = driver.findElement(By.cssSelector("form#login_form div#error.loginError")).getText();
-        String xpassText = driver.findElement(By.xpath("//*[@id='error']")).getText();
-        String xpassText2 = driver.findElement(By.xpath("//form/div[@class='loginError']")).getText();
-
-        System.out.println(cssText + "21");
-        System.out.println(cssText2 + "22");
-        System.out.println(xpassText + "24");
-        System.out.println(xpassText2 + "25");
 
 
     }
