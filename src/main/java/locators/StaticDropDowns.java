0cb7eb4391f10656041e1e442964c49807562893
andreybannikov <andrewnnov@yandex.ru>
2020-11-01 09:53:18 +0300

    StaticDropDowns practice

--- src/main/java/locators/StaticDropDowns.java
+++ src/main/java/locators/StaticDropDowns.java
@@ -13,12 +13,25 @@ public class StaticDropDowns {
         System.setProperty("webdriver.chrome.driver", "C:\\Projects\\selwd\\driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
         driver.get("https://www.spicejet.com/");
-        Select select = new Select(driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")));
-        select.selectByValue("USD");
-        select.selectByVisibleText("AED");
-        select.selectByIndex(2);
+//        Select select = new Select(driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")));
+//        select.selectByValue("USD");
+//        select.selectByVisibleText("AED");
+//        select.selectByIndex(2);
+
+        driver.findElement(By.id("divpaxinfo")).click();
+
+        Select selectAdult = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Adult")));
+        selectAdult.selectByValue("2");
+
+        Select selectChild = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Child")));
+        selectChild.selectByVisibleText("4");
+
+        Select selectInfant = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Infant")));
+        selectInfant.selectByValue("2");
+
 
     }
 }
