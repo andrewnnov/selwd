c2868c970163779192dc2e6880fd53a84fef8546
andreybannikov <andrewnnov@yandex.ru>
2020-11-04 16:42:14 +0300

    practice CheckBox with Assert from TestNG lib

--- src/main/java/locators/CheckBoxClass.java
+++ src/main/java/locators/CheckBoxClass.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -17,14 +18,22 @@ public class CheckBoxClass {
         driver.manage().window().maximize();
 
         driver.get("https://www.spicejet.com/");
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+
         driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).click(); //css selector
-        System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+        //System.out.println();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//input[contains(@id, 'friendsandfamily')]")).isSelected());
 
         driver.findElement(By.xpath("//input[contains(@id, 'friendsandfamily')]")).click(); //xpass selector
-        System.out.println(driver.findElement(By.xpath("//input[contains(@id, 'friendsandfamily')]")).isSelected());
+        //System.out.println();
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
 
         //count of number chechboxes
         List<WebElement> listOfCheckBoxes = driver.findElements(By.xpath("//input[@type='checkbox']"));
+
+        Assert.assertEquals(listOfCheckBoxes.size(), 6);
+
         System.out.println(listOfCheckBoxes.size());
 
 //        for (WebElement elCheckBox: listOfCheckBoxes) {
---
+++ src/main/java/locators/CheckBoxPractice.java
@@ -0,0 +1,28 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+public class CheckBoxPractice {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Projects\\selwd\\driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        Assert.assertFalse(driver.findElement(By.xpath("//input[@id='checkBoxOption1']")).isSelected());
+        driver.findElement(By.xpath("//input[@id='checkBoxOption1']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//input[@id='checkBoxOption1']")).isSelected());
+
+        System.out.println(driver.findElements(By.xpath("//input[@type='checkbox']")).size());
+    }
+
+
+}
