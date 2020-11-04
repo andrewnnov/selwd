09d53d7e03c7b9cc4a08cb99bd63170738a60867
andreybannikov <andrewnnov@yandex.ru>
2020-11-04 15:46:34 +0300

    work with checkBox

---
+++ src/main/java/locators/CheckBoxClass.java
@@ -0,0 +1,40 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class CheckBoxClass {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Projects\\selwd\\driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("https://www.spicejet.com/");
+        driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).click(); //css selector
+        System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+
+        driver.findElement(By.xpath("//input[contains(@id, 'friendsandfamily')]")).click(); //xpass selector
+        System.out.println(driver.findElement(By.xpath("//input[contains(@id, 'friendsandfamily')]")).isSelected());
+
+        //count of number chechboxes
+        List<WebElement> listOfCheckBoxes = driver.findElements(By.xpath("//input[@type='checkbox']"));
+        System.out.println(listOfCheckBoxes.size());
+
+//        for (WebElement elCheckBox: listOfCheckBoxes) {
+//            elCheckBox.click();
+//        }
+
+//        for (int i = 1; i < listOfCheckBoxes.size(); i++) {
+//            listOfCheckBoxes.get(i).click();
+//
+//        }
+
+    }
+}
