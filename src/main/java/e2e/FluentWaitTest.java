7e373a9c792ccb25dff5b1c53c30f799463a4a76
andreybannikov <andrewnnov@yandex.ru>
2020-11-15 22:02:55 +0300

    practice fluentWait

--- src/main/java/e2e/FluentWaitTest.java
+++ src/main/java/e2e/FluentWaitTest.java
@@ -28,9 +28,15 @@ public class FluentWaitTest {
 
         WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
             public WebElement apply(WebDriver driver) {
-                return driver.findElement(By.xpath("//div[@id='finish']/h4"));
+                if(driver.findElement(By.xpath("//div[@id='finish']/h4")).isDisplayed()) {
+                    return driver.findElement(By.xpath("//div[@id='finish']/h4"));
+                } else
+                    return null;
             }
         });
+
+        System.out.println(driver.findElement(By.xpath("//div[@id='finish']/h4")).isDisplayed());
+        System.out.println(driver.findElement(By.xpath("//div[@id='finish']/h4")).getText());
 
     }
 }
