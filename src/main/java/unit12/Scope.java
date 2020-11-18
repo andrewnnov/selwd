6a19086f0f4576d9721adee14681f8dba442af9b
andreybannikov <andrewnnov@yandex.ru>
2020-11-18 13:22:33 +0300

    click to each link on the page

---
+++ src/main/java/unit12/Scope.java
@@ -0,0 +1,51 @@
+package unit12;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Scope {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Projects\\selwd\\driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+
+        //1. Give me the count of links
+        //a
+        System.out.println(driver.findElements(By.tagName("a")).size());
+
+
+        //create sub driver for footer section
+        WebElement footDriver = driver.findElement(By.id("gf-BIG"));
+        System.out.println(footDriver.findElements(By.tagName("a")).size());
+
+
+        WebElement columnDriver = footDriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+        System.out.println(columnDriver.findElements(By.tagName("a")).size());
+
+
+        //click on each link the column and check if page are opening
+        for(int i = 0; i < columnDriver.findElements(By.tagName("a")).size(); i++ ) {
+
+            String clickOnLink = Keys.chord(Keys.CONTROL, Keys.ENTER);
+            columnDriver.findElements(By.tagName("a")).get(i).sendKeys(clickOnLink);
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
