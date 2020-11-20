9d492d8e29178f934b6e0e7b426ee8bddc7ae02a
andreybannikov <andrewnnov@yandex.ru>
2020-11-20 17:56:35 +0300

    work with table part 2

--- src/main/java/unit13/TableExample.java
+++ src/main/java/unit13/TableExample.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,8 +26,21 @@ public class TableExample {
 
         int count = table.findElements(By.xpath("//div[@id='innings_1']/div[@class='cb-col cb-col-100 cb-ltst-wgt-hdr'][1]//div[@class='cb-col cb-col-8 text-right text-bold']")).size();
         System.out.println("count - " + count);
+
+        int sum = 0;
         for (int i = 1; i < count; i++) {
             System.out.println(table.findElements(By.xpath("//div[@id='innings_1']/div[@class='cb-col cb-col-100 cb-ltst-wgt-hdr'][1]//div[@class='cb-col cb-col-8 text-right text-bold']")).get(i).getText());
+            String value = table.findElements(By.xpath("//div[@id='innings_1']/div[@class='cb-col cb-col-100 cb-ltst-wgt-hdr'][1]//div[@class='cb-col cb-col-8 text-right text-bold']")).get(i).getText();
+            int valueInteger = Integer.parseInt(value);
+            sum = sum + valueInteger;
         }
+
+        System.out.println("Extras - " + driver.findElement(By.xpath("//div[@id='innings_1']/div[@class='cb-col cb-col-100 cb-ltst-wgt-hdr'][1]//div[text()='Extras']/following-sibling::div[1]")).getText());
+        int extras = Integer.parseInt(driver.findElement(By.xpath("//div[@id='innings_1']/div[@class='cb-col cb-col-100 cb-ltst-wgt-hdr'][1]//div[text()='Extras']/following-sibling::div[1]")).getText());
+        System.out.println("Total - " + driver.findElement(By.xpath("//div[@id='innings_1']/div[@class='cb-col cb-col-100 cb-ltst-wgt-hdr'][1]//div[text()='Total']/following-sibling::div[1]")).getText());
+        int total = Integer.parseInt(driver.findElement(By.xpath("//div[@id='innings_1']/div[@class='cb-col cb-col-100 cb-ltst-wgt-hdr'][1]//div[text()='Total']/following-sibling::div[1]")).getText());
+        sum = sum + extras;
+        System.out.println("Sum = " + sum );
+        Assert.assertEquals(sum, total);
     }
 }
