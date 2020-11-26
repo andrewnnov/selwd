d3064c304f10c4ee2ba7db35f570dd8806f138e6
andreybannikov <andrewnnov@yandex.ru>
2020-11-26 19:45:51 +0300

    how to create screenshots

--- src/main/java/unit14/MaxBrowserExample.java
+++ src/main/java/unit14/MaxBrowserExample.java
@@ -1,8 +1,13 @@
 package unit14;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class MaxBrowserExample {
@@ -14,11 +19,15 @@ public class MaxBrowserExample {
         driver.manage().window().maximize();
         //del all cookies
         driver.manage().deleteAllCookies();
-
         //delete specific
         driver.manage().deleteCookieNamed("sessionKey");
-
-
         driver.get("https://www.google.com");
+
+        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenshot, new File("C:\\Projects\\selwd\\screenshots\\scr.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
