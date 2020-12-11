2b10832a723e53bd96b4a864adb184893973d1fa
andreybannikov <andrewnnov@yandex.ru>
2020-12-11 10:49:28 +0300

    annotation before and after testng

--- src/main/java/testngtutorial/test/Day1.java
+++ src/main/java/testngtutorial/test/Day1.java
@@ -1,16 +1,18 @@
 package testngtutorial.test;
 
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
 
 public class Day1 {
+
+
+
 
     @Test
     public void demo() {
         System.out.println("hello");
     }
 
-    @AfterSuite
+    @Test
     public void SecondTest() {
         System.out.println("bye");
     }
--- src/main/java/testngtutorial/test/Day3.java
+++ src/main/java/testngtutorial/test/Day3.java
@@ -1,5 +1,8 @@
 package testngtutorial.test;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Day3 {
@@ -33,4 +36,28 @@ public class Day3 {
         //rest API
         System.out.println("API");
     }
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("I am number one");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("I am number one last");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("before method before day 3 class");
+    }
+
+    @BeforeMethod
+    public void afterMethod() {
+        System.out.println("after method before day 3 class");
+    }
+
+
+
+
 }
